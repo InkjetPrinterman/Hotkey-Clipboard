e1967ea148d55917abe99237fa3dd244909688e4
InkjetPrinterman <deligentissima@tutamail.com>
2026-09-09 18:20:16 -0500

    Update ClipboardHelper.cs

--- ClipboardHelper.cs
+++ ClipboardHelper.cs
@@ -34,5 +34,37 @@ namespace HotkeyClipboard
             }
             return false;
         }
+    public static async Task<string?> TryGetTextAsync()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                try
+                {
+                    return Clipboard.ContainsText() ? Clipboard.GetText() : null;
+                }
+                    catch (ExternalException)
+                {
+                await Task.Delay(40);
+            }
+        }
+                return null;
     }
+
+    public static async Task<bool> TrySetTextAsync(string text)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                try
+                {
+                    Clipboard.SetText(text);
+                    return true;
+                }
+                    catch (ExternalException)
+            {
+                await Task.Delay(40);
+        }
+    }
+                return false;
+    }
+ }
 }
